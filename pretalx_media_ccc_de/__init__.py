465759ac3cf8de9b9a4b7d67e2815b0a44be91a9
Tobias Kunze <r@rixx.de>
2018-11-24 17:49:42 +0100

    Initial commit

---
+++ pretalx_media_ccc_de/__init__.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = 'pretalx_media_ccc_de'
+    verbose_name = 'media.ccc.de recordings'
+
+    class PretalxPluginMeta:
+        name = ugettext_lazy('media.ccc.de recordings')
+        author = 'Tobias Kunze'
+        description = ugettext_lazy('Pull recordings from media.ccc.de and embed them in talk pages')
+        visible = True
+        version = '0.0.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretalx_media_ccc_de.PluginApp'
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from distutils.command.build import build
+
+from django.core import management
+from setuptools import setup, find_packages
+
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
+
+
+class CustomBuild(build):
+    def run(self):
+        management.call_command('compilemessages', verbosity=1, interactive=False)
+        build.run(self)
+
+
+cmdclass = {
+    'build': CustomBuild
+}
+
+
+setup(
+    name='pretalx-media-ccc-de',
+    version='0.0.0',
+    description='Pull recordings from media.ccc.de and embed them in talk pages',
+    long_description=long_description,
+    url='https://github.com/pretalx/pretalx-media-ccc-de.git',
+    author='Tobias Kunze',
+    author_email='r@rixx.de',
+    license='Apache Software License',
+
+    install_requires=[],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    cmdclass=cmdclass,
+    entry_points="""
+[pretalx.plugin]
+pretalx_media_ccc_de=pretalx_media_ccc_de:PretalxPluginMeta
+""",
+)
