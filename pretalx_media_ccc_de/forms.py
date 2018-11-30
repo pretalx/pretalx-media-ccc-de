9382060c38b90cfd8c3d5414a05c09b7cb59578f
Tobias Kunze <r@rixx.de>
2018-11-30 23:59:45 +0100

    Allow to edit fields manually

--- pretalx_media_ccc_de/forms.py
+++ pretalx_media_ccc_de/forms.py
@@ -10,7 +10,26 @@ class MediaCCCDeSettingsForm(HierarkeyForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         event = kwargs.get('obj')
-        url = 'https://media.ccc.de/public/conferences/{slug}'.format(slug=event.settings.media_ccc_de_id or event.slug)
-        self.fields['media_ccc_de_id'].help_text = _('The slug or ID used for your event in the media.ccc.de API – for example your event slug. Your event\'s data has to be available at <a href="{url}">{url}</a>').format(url=url)
+        url = 'https://media.ccc.de/public/conferences/{slug}'.format(
+            slug=event.settings.media_ccc_de_id or event.slug
+        )
+        self.fields['media_ccc_de_id'].help_text = _(
+            'The slug or ID used for your event in the media.ccc.de API – for example your event slug. Your event\'s data has to be available at <a href="{url}">{url}</a>'
+        ).format(url=url)
         if not event.settings.media_ccc_de_id:
             self.fields['media_ccc_de_id'].default = event.slug
+
+
+class MediaCCCDeUrlForm(forms.Form):
+
+    media_ccc_de_url = forms.URLField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        self.submission = kwargs.pop('submission')
+        initial = kwargs.get('initial', dict())
+        initial['media_ccc_de_url'] = self.submission.event.settings.get(
+            f'media_ccc_de_url_{self.submission.code}'
+        )
+        kwargs['initial'] = initial
+        super().__init__(*args, **kwargs)
+        self.fields['media_ccc_de_url'].label = self.submission.title
--- pretalx_media_ccc_de/views.py
+++ pretalx_media_ccc_de/views.py
@@ -2,8 +2,9 @@ from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import FormView
 from pretalx.common.mixins.views import PermissionRequired
+from pretalx.submission.models import Submission, SubmissionStates
 
-from .forms import MediaCCCDeSettingsForm
+from .forms import MediaCCCDeSettingsForm, MediaCCCDeUrlForm
 from .recording import MediaCCCDe
 
 
@@ -20,10 +21,31 @@ class MediaCCCDeSettings(PermissionRequired, FormView):
         return super().form_valid(form)
 
     def post(self, request, *args, **kwargs):
-        if request.POST.get('action', 'save') == 'regenerate':
-            messages.success(request, _('Looking for new talks – this may take a while.'))
+        action = request.POST.get('action', 'save')
+        if action == 'regenerate':
+            messages.success(
+                request, _('Looking for new talks – this may take a while.')
+            )
             MediaCCCDe(request.event).fill_recording_urls()
-            
+
+        elif action.startswith('url'):
+            code = action.lstrip('url_')
+            try:
+                submission = request.event.submissions.get(code=code)
+            except Submission.DoesNotExist:
+                messages.error(_('Could not find this talk.'))
+                return super().post(request, *args, **kwargs)
+
+            form = MediaCCCDeUrlForm(request.POST, submission=submission)
+            if not form.is_valid():
+                messages.error(form.errors)
+            else:
+                request.event.settings.set(
+                    f'media_ccc_de_url_{submission.code}',
+                    form.cleaned_data['media_ccc_de_url'],
+                )
+                messages.success(_('The URL for this talk was overridden.'))
+
         return super().post(request, *args, **kwargs)
 
     def get_object(self):
@@ -31,8 +53,12 @@ class MediaCCCDeSettings(PermissionRequired, FormView):
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        return {
-            'obj': self.request.event,
-            'attribute_name': 'settings',
-            **kwargs
-        }
+        return {'obj': self.request.event, 'attribute_name': 'settings', **kwargs}
+
+    def get_context_data(self):
+        kwargs = super().get_context_data()
+        kwargs['url_forms'] = [
+            MediaCCCDeUrlForm(submission=submission)
+            for submission in self.request.event.talks
+        ]
+        return kwargs
