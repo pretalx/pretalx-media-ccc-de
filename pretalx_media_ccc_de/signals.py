a28a4f9c98429033a0e045342477c25685f0ebd6
Tobias Kunze <r@rixx.de>
2019-06-14 01:29:34 +0200

    Use django_scopes

--- pretalx_media_ccc_de/signals.py
+++ pretalx_media_ccc_de/signals.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from django.dispatch import receiver
 from django.urls import reverse
 from django.utils.timezone import now
+from django_scopes import scopes_disabled, scope
 from pretalx.agenda.signals import register_recording_provider
 from pretalx.common.signals import periodic_task
 from pretalx.event.models import Event
@@ -18,14 +19,16 @@ def media_ccc_de_provider(sender, **kwargs):
 
 @receiver(periodic_task)
 def gather_media_ccc_de_urls(**kwargs):
-    active_events = Event.objects.filter(plugins__icontains='media_ccc_de')
+    with scopes_disabled():
+        active_events = Event.objects.filter(plugins__icontains='media_ccc_de')
     for event in active_events:
-        if 'media_ccc_de' not in event.plugin_list or now().date() < event.date_from:
-            continue
-        last_check = event.settings.media_ccc_de_check
-        event_active = (now().date() - event.date_to) <= timedelta(days=7)
-        if not last_check or (now() - last_check > timedelta(hours=1) and event_active):
-            MediaCCCDe(event).fill_recording_urls()
+        with scope(event=event):
+            if 'media_ccc_de' not in event.plugin_list or now().date() < event.date_from:
+                continue
+            last_check = event.settings.media_ccc_de_check
+            event_active = (now().date() - event.date_to) <= timedelta(days=7)
+            if not last_check or (now() - last_check > timedelta(hours=1) and event_active):
+                MediaCCCDe(event).fill_recording_urls()
 
 
 @receiver(nav_event_settings)
--- pretalx_media_ccc_de/tasks.py
+++ pretalx_media_ccc_de/tasks.py
@@ -2,6 +2,7 @@ import json
 from contextlib import suppress
 
 import requests
+from django_scopes import scopes_disabled, scope
 from pretalx.celery_app import app
 from pretalx.event.models import Event
 from pretalx.submission.models import Submission
@@ -10,42 +11,44 @@ from pretalx.submission.models import Submission
 @app.task()
 def task_refresh_recording_urls(event_slug):
     try:
-        event = Event.objects.get(slug__iexact=event_slug)
+        with scopes_disabled():
+            event = Event.objects.get(slug__iexact=event_slug)
     except Event.DoesNotExist:
         return
 
-    if not event.settings.media_ccc_de_id:
-        event.settings.media_ccc_de_id = event.slug
+    with scope(event=event):
+        if not event.settings.media_ccc_de_id:
+            event.settings.media_ccc_de_id = event.slug
 
-    response = requests.get(
-        f'https://media.ccc.de/public/conferences/{event.settings.media_ccc_de_id}'
-    )
-    if not response.status_code == 200:
-        return None
+        response = requests.get(
+            f'https://media.ccc.de/public/conferences/{event.settings.media_ccc_de_id}'
+        )
+        if not response.status_code == 200:
+            return None
 
-    structure = json.loads(response.content.decode())
-    for talk in structure.get('events', []):
-        if talk.get('frontend_link'):
-            submission = None
-            link = talk.get('link')
-            if link:
-                with suppress(Submission.DoesNotExist):
-                    submission = Submission.objects.get(
-                        event=event,
-                        code__iexact=talk['link']
-                        .rstrip('/')
-                        .rsplit('/', maxsplit=1)[-1],
-                    )
-            if not submission:
-                with suppress(Submission.DoesNotExist):
-                    submission = Submission.objects.get(
-                        event=event, pk__iexact=talk['slug'].split('-')[1]
-                    )
-                with suppress(Submission.DoesNotExist):
-                    submission = Submission.objects.get(
-                        event=event, code__iexact=talk['slug'].split('-')[1]
-                    )
-            if submission:
-                key = f'media_ccc_de_url_{submission.code}'
-                if not event.settings.get(key):
-                    event.settings.set(key, talk['frontend_link'])
+        structure = json.loads(response.content.decode())
+        for talk in structure.get('events', []):
+            if talk.get('frontend_link'):
+                submission = None
+                link = talk.get('link')
+                if link:
+                    with suppress(Submission.DoesNotExist):
+                        submission = Submission.objects.get(
+                            event=event,
+                            code__iexact=talk['link']
+                            .rstrip('/')
+                            .rsplit('/', maxsplit=1)[-1],
+                        )
+                if not submission:
+                    with suppress(Submission.DoesNotExist):
+                        submission = Submission.objects.get(
+                            event=event, pk__iexact=talk['slug'].split('-')[1]
+                        )
+                    with suppress(Submission.DoesNotExist):
+                        submission = Submission.objects.get(
+                            event=event, code__iexact=talk['slug'].split('-')[1]
+                        )
+                if submission:
+                    key = f'media_ccc_de_url_{submission.code}'
+                    if not event.settings.get(key):
+                        event.settings.set(key, talk['frontend_link'])
