1546d78d847fa26a3a4320d5479e49b3199a7c47
Tobias Kunze <r@rixx.de>
2019-06-21 16:50:05 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ cmdclass = {'build': CustomBuild}
 
 setup(
     name='pretalx-media-ccc-de',
-    version='0.2.0',
+    version='0.3.0',
     description='Pull recordings from media.ccc.de and embed them in talk pages',
     long_description=long_description,
     url='https://github.com/pretalx/pretalx-media-ccc-de.git',
