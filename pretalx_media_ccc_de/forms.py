af8bfa6599d6454f3efee00371632a5e6350de39
Tobias Kunze <r@rixx.de>
2023-09-26 16:48:00 +0200

    Show link to video and to talk page

--- pretalx_media_ccc_de/forms.py
+++ pretalx_media_ccc_de/forms.py
@@ -24,24 +24,26 @@ class MediaCCCDeUrlForm(forms.Form):
         if not event or not event.current_schedule:
             return super().__init__(*args, **kwargs)
         self.event = event
+        super().__init__(*args, **kwargs)
 
         self.talks = (
             event.current_schedule.talks.all()
             .filter(is_visible=True, submission__isnull=False)
             .order_by("start")
         )
-        initial = kwargs.get("initial", dict())
+        s = _("Go to video.")
+        p = _("Go to talk page.")
         for talk in self.talks:
-            initial[f"media_ccc_de_url_{talk.submission.code}"] = event.settings.get(
-                f"media_ccc_de_url_{talk.submission.code}"
-            )
-        kwargs["initial"] = initial
-        super().__init__(*args, **kwargs)
-        for talk in self.talks:
+            initial = event.settings.get(f"media_ccc_de_url_{talk.submission.code}")
+            help_text = f'<a href="{talk.submission.urls.public.full()}">{p}</a>'
+            if initial:
+                help_text += f' | <a href="{initial}">{s}</a>'
             self.fields[f"media_ccc_de_url_{talk.submission.code}"] = forms.URLField(
                 required=False,
                 label=talk.submission.title,
                 widget=forms.URLInput(attrs={"placeholder": ""}),
+                initial=initial,
+                help_text=help_text,
             )
 
     def save(self):
