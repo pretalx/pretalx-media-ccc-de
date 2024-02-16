ff467d512586ec56a5d32438877ac53dd408253f
Tobias Kunze <r@rixx.de>
2024-02-16 15:22:01 +0100

    Update translations

--- pretalx_media_ccc_de/urls.py
+++ pretalx_media_ccc_de/urls.py
@@ -1,11 +1,11 @@
 from django.urls import re_path
-from pretalx.event.models.event import SLUG_CHARS
+from pretalx.event.models.event import SLUG_REGEX
 
 from .views import MediaCCCDeSettings
 
 urlpatterns = [
     re_path(
-        rf"^orga/event/(?P<event>[{SLUG_CHARS}]+)/settings/p/media.ccc.de/$",
+        rf"^orga/event/(?P<event>{SLUG_REGEX})/settings/p/media.ccc.de/$",
         MediaCCCDeSettings.as_view(),
         name="settings",
     )
